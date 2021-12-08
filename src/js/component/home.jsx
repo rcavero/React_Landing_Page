9883041a55fe5c7e860877a1bc5bc1c8971eff1c
rcavero <ramoncaveroaras@gmail.com>
2021-12-08 00:10:19 +0000

    First changes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+
+import Navbar from "./navbar.jsx";
+import Jumbotron from "./jumbotron.jsx";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -6,19 +11,13 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-fluid p-0">
+			<Navbar />
+			<div className="body container-fluid">
+				<Jumbotron />
+				{/* <Card /> */}
+			</div>
+			{/* <Footer /> */}
 		</div>
 	);
 };
---
+++ src/js/component/jumbotron.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+
+const Jumbotron = (props) => {
+	return (
+		<div class="container-fluid py-5">
+			<h1 class="display-5 fw-bold">Custom jumbotron</h1>
+			<p class="col-md-8 fs-4">
+				Using a series of utilities, you can create this jumbotron, just
+				like the one in previous versions of Bootstrap. Check out the
+				examples below for how you can remix and restyle it to your
+				liking.
+			</p>
+			<button class="btn btn-primary btn-lg" type="button">
+				Example button
+			</button>
+		</div>
+	);
+};
+
+export default Jumbotron;
